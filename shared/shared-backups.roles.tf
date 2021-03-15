06d756e3c3ee1658152383eac9bf2c20563931ca
Justin <JEStaubach@users.noreply.github.com>
2021-03-15 19:35:52 -0400

    Pull project roles into their own file.

---
+++ shared/shared-backups.roles.tf
@@ -0,0 +1,24 @@
+resource "google_organization_iam_custom_role" "shared-backups-test-role-svc-check-snapshots" {
+  role_id     = "shared-backups-test-role-svc-check-snapshots"
+  org_id      = var.organization_id
+  title       = "shared-backups-test-role-svc-check-snapshots"
+  description = "Role / permissions to assign to service account for automatically setting up disk snapshots. (test)"
+  permissions = [
+    "compute.disks.list",
+    "compute.snapshots.list",
+    "compute.instances.list",
+    "compute.regions.list",
+    "compute.zones.list",
+    "compute.disks.addResourcePolicies",
+    "compute.resourcePolicies.create",
+    "compute.resourcePolicies.get",
+    "compute.resourcePolicies.list",
+    "compute.resourcePolicies.use",
+    "resourcemanager.organizations.get",
+    "resourcemanager.folders.get",
+    "resourcemanager.folders.list",
+    "resourcemanager.projects.get",
+    "resourcemanager.projects.list",
+    "storage.objects.create"
+  ]
+}
