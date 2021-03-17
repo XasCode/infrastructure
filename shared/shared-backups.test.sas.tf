1354295b9bc59296ac4aab5941f81a13ba32379c
Justin <JEStaubach@users.noreply.github.com>
2021-03-16 21:12:59 -0400

    create check snapshots svc account and bind it at org level

---
+++ shared/shared-backups.test.sas.tf
@@ -0,0 +1,14 @@
+resource "google_service_account" "shared-backups-test-svc-check-snapshots" {
+  account_id   = "svc-check-snapshots-${random_id.project.hex}"
+  display_name = "shared-backups-test-svc-check-snapshots"
+  project      = google_project.backups-test.id
+}
+
+resource "google_organization_iam_binding" "shared-backups-test-bind-check-snapshots" {
+  org_id  = var.organization_id
+  role    = "roles/${google_organization_iam_custom_role.shared-backups-test-role-svc-check-snapshots.role_id}"
+
+  members = [
+    "serviceAccount:${google_service_account.shared-backups-test-svc-check-snapshots.email}"
+  ]
+}
