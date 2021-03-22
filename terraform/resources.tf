b05732b7be6b7766e3f8dc5627586db4b7595171
Justin <JEStaubach@users.noreply.github.com>
2021-03-21 23:56:42 -0400

    Add snapshots project.

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,11 +1,11 @@
 module "org" {
-  source = "github.com/xascode/modules/org"
+  source = "github.com/xascode/modules//org?ref=v0.1.0-alpha.1"
   
   name   = var.organization_name
 }
 
 module "env" {
-  source = "github.com/xascode/modules/env"
+  source = "github.com/xascode/modules//env?ref=v0.1.0-alpha.1"
 
   name   = var.environment
   parent = {
@@ -15,7 +15,7 @@ module "env" {
 }
 
 module "corp" {
-  source = "github.com/xascode/modules/ou"
+  source = "github.com/xascode/modules//ou?ref=v0.1.0-alpha.1"
 
   name   = "corp"
   parent = {
@@ -25,11 +25,23 @@ module "corp" {
 }
 
 module "snapshots" {
-  source = "github.com/xascode/modules/prj_container"
+  source = "github.com/xascode/modules//prj_container?ref=v0.1.0-alpha.1"
 
-  name = "snapshots"
+  name   = "snapshots"
   parent = {
     name = module.corp.name
     path = module.corp.path
   }
+}
+
+module "snapshots_project" {
+  source = "github.com/xascode/modules//project?ref=v0.1.0-alpha.3"
+
+  name   = "snapshots"
+  parent = {
+    name = module.snapshots.name
+    path = module.snapshots.path
+  }
+
+  billing_account = var.billing_account
 }
