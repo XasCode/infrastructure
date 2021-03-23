40b614754eb16ea7adc54926d7eaded2928484aa
Justin <JEStaubach@users.noreply.github.com>
2021-03-22 20:38:48 -0400

    Instead of an independent snaphsots project, its been moved into every OU along with a security project.

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,11 +1,11 @@
 module "org" {
-  source = "github.com/xascode/modules//org?ref=v0.1.0-alpha.1"
+  source = "github.com/xascode/tf_modules//org?ref=v0.1.0-alpha.1"
   
   name   = var.organization_name
 }
 
 module "env" {
-  source = "github.com/xascode/modules//env?ref=v0.1.0-alpha.1"
+  source = "github.com/xascode/tf_modules//env?ref=v0.1.0-alpha.1"
 
   name   = var.environment
   parent = {
@@ -15,32 +15,13 @@ module "env" {
 }
 
 module "corp" {
-  source = "github.com/xascode/modules//ou?ref=v0.1.0-alpha.1"
+  source = "github.com/xascode/tf_modules//ou?ref=v0.1.0-alpha.5"
+  //source = "./modules/ou"
 
   name   = "corp"
   parent = {
     name = module.env.name
     path = module.env.path
-  }
-}
-
-module "snapshots" {
-  source = "github.com/xascode/modules//prj_container?ref=v0.1.0-alpha.1"
-
-  name   = "snapshots"
-  parent = {
-    name = module.corp.name
-    path = module.corp.path
-  }
-}
-
-module "snapshots_project" {
-  source = "github.com/xascode/modules//project?ref=v0.1.0-alpha.3"
-
-  name   = "snapshots"
-  parent = {
-    name = module.snapshots.name
-    path = module.snapshots.path
   }
 
   billing_account = var.billing_account
