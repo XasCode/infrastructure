c8949ffde3c17e41c8e00c33d1944b0ad42acdf1
Justin <JEStaubach@users.noreply.github.com>
2021-06-27 10:40:55 -0400

    use service account for function

--- terraform/src/index.js
+++ terraform/src/index.js
@@ -27,9 +27,11 @@ exports.helloPubSub = async (event, _context) => {
   async function getProjectId() {
     const compute = new Compute();
     const thisPrj = compute.project();
+    console.log(`${JSON.stringify(thisPrj)}`);
     const prjData = await thisPrj.get();
+    console.log(`${JSON.stringify(prjData)}`);
     const prj = prjData[0];
-    console.log(JSON.stringify(prj));
+    console.log(`${JSON.stringify(prj)}`);
     return prj;
   }
 
