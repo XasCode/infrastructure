65e199e885f191cd3c72d329aad192f778abfef4
Justin <JEStaubach@users.noreply.github.com>
2021-08-03 20:58:55 -0400

    save-progress

--- terraform/vendor/modules/choreographer/main.lb.tf
+++ terraform/vendor/modules/choreographer/main.lb.tf
@@ -1,5 +1,6 @@
 resource "google_compute_region_network_endpoint_group" "cloudrun_neg" {
   name                  = "cloudrun-neg"
+  project               = module.project.id
   network_endpoint_type = "SERVERLESS"
   region                = "us-central1"
   cloud_run {
