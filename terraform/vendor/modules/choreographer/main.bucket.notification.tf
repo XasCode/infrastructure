1adfb62b1ee68c85e977c9accc016d7ae6f118e3
Justin <JEStaubach@users.noreply.github.com>
2021-08-09 21:10:41 -0400

    save progress

---
+++ terraform/vendor/modules/choreographer/main.bucket.notification.tf
@@ -0,0 +1,41 @@
+resource "google_storage_notification" "notification" {
+  count = var.environment == "devl" ? length(var.managed) : 0
+  project = var.managed[count.index].id
+  bucket         = google_storage_bucket.bucket[count.index].name
+  payload_format = "JSON_API_V1"
+  topic          = google_pubsub_topic.topic[count.index].id
+  event_types    = ["OBJECT_FINALIZE", "OBJECT_METADATA_UPDATE"]
+  custom_attributes = {
+    new-attribute = "new-attribute-value"
+  }
+  depends_on = [google_pubsub_topic_iam_binding.binding]
+}
+
+// Enable notifications by giving the correct IAM permission to the unique service account.
+
+data "google_storage_project_service_account" "gcs_account" {
+  count = var.environment == "devl" ? length(var.managed) : 0
+  project = var.managed[count.index].id
+}
+
+resource "google_pubsub_topic_iam_binding" "binding" {
+  count = var.environment == "devl" ? length(var.managed) : 0
+  project = var.managed[count.index].id
+  topic   = google_pubsub_topic.topic[count.index].id
+  role    = "roles/pubsub.publisher"
+  members = ["serviceAccount:${data.google_storage_project_service_account.gcs_account[count.index].email_address}"]
+}
+
+// End enabling notifications
+
+resource "google_storage_bucket" "bucket" {
+  count = var.environment == "devl" ? length(var.managed) : 0
+  project = var.managed[count.index].id
+  name = "source_bucket"
+}
+
+resource "google_pubsub_topic" "topic" {
+  count = var.environment == "devl" ? length(var.managed) : 0
+  project = var.managed[count.index].id
+  name = "source_bucket_notification_topic"
+}
