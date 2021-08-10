3b671a6fa62bd67aab0edc7591a7293aedb15f97
Justin <JEStaubach@users.noreply.github.com>
2021-08-09 21:44:46 -0400

    save progress

--- terraform/vendor/modules/choreographer/main.services.tf
+++ terraform/vendor/modules/choreographer/main.services.tf
@@ -8,24 +8,24 @@ resource "google_project_service" "sourcerepo" {
   }
   disable_dependent_services = true
 }
-/*
-resource "google_project_service" "cloud_functions" {
-  count        = contains(var.envs, var.environment) ? 1 : 0
-
-  project = module.project.id
-  service = "cloudfunctions.googleapis.com"
-  timeouts {
-    create = "3m"
-    update = "6m"
-  }
-  disable_dependent_services = true
-}
 
 resource "google_project_service" "cloud_build" {
   count        = contains(var.envs, var.environment) ? 1 : 0
 
   project = module.project.id
   service = "cloudbuild.googleapis.com"
+  timeouts {
+    create = "3m"
+    update = "6m"
+  }
+  disable_dependent_services = true
+}
+/*
+resource "google_project_service" "cloud_functions" {
+  count        = contains(var.envs, var.environment) ? 1 : 0
+
+  project = module.project.id
+  service = "cloudfunctions.googleapis.com"
   timeouts {
     create = "3m"
     update = "6m"
