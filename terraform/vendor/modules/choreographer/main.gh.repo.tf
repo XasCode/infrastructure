626b1e5f12984ea745a63b0f5b3c349ff540dcdb
Justin <JEStaubach@users.noreply.github.com>
2021-08-15 15:42:51 -0400

    save progress

--- terraform/vendor/modules/choreographer/main.gh.repo.tf
+++ terraform/vendor/modules/choreographer/main.gh.repo.tf
@@ -83,6 +83,6 @@ resource "github_repository_file" "gh_repo_file_archive" {
 
 resource "github_branch" "branch" {
   count = var.environment == "devl" ? 0 : length(var.managed)
-  repository = data.github_repository.repository[count.index]
+  repository = data.github_repository.repository[count.index].full_name
   branch     = local.branch
 }
